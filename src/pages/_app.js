7358989a216d1f9d6b923630c7905d6eaed0af40
Alexa Kruckenberg <mouldy.k@gmail.com>
2023-09-21 18:44:28 -0700

    add scroll to top capabilities

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import "./global.css";
 import Header from "../components/shared/Header.js";
 import Footer from "../components/shared/Footer.js";
 import Background from "../components/shared/Background.js";
+import ScrollToTop from "../components/shared/ScrollToTop";
 import {
   ContentContainer,
   PageContainer,
@@ -15,11 +16,13 @@ export default function App({ Component, pageProps }) {
     <PageContainer>
       <Header />
       <Background />
-      <ContentContainer>
-        <Component {...pageProps} />
-        <Filler />
-        <Footer />
-      </ContentContainer>
+      <ScrollToTop>
+        <ContentContainer id="content-container">
+          <Component {...pageProps} />
+          <Filler />
+          <Footer />
+        </ContentContainer>
+      </ScrollToTop>
       <BackgroundGrass />
     </PageContainer>
   );
--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -27,7 +27,7 @@ export default function PostPage({ source }) {
   return (
     <div>
       <Head>
-        <title>{source.frontmatter.title}</title>
+        <title>{source?.frontmatter?.title}</title>
       </Head>
       <BlogPost source={source}>
         <MDXRemote
@@ -40,7 +40,7 @@ export default function PostPage({ source }) {
   );
 }
 export async function getStaticPaths() {
-  return { paths: [], fallback: "blocking" };
+  return { paths: [], fallback: false };
 }
 
 export async function getStaticProps(ctx) {
