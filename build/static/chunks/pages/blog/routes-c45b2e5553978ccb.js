19e88cfa797cd6888462acd26fdbffe23b648832
Alexa Kruckenberg <mouldy.k@gmail.com>
2023-09-21 17:47:23 -0700

    try to get next build working

---
+++ build/static/chunks/pages/blog/routes-c45b2e5553978ccb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[685],{4335:function(){},5291:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/routes",function(){return _(4335)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=5291)}),_N_E=n.O()}]);
---
+++ build/static/chunks/pages/constants-71d0a8f617b4f2a7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[148],{6845:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/constants",function(){return u(9457)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=6845)}),_N_E=n.O()}]);
