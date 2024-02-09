ba5890a19cd35e68c9554e08d5e80f3f21f1188a
Alexa Kruckenberg <mouldy.k@gmail.com>
2024-02-08 16:21:36 -0800

    add postgres test table

---
+++ src/pages/api/create-test-table.js
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { sql } from "@vercel/postgres";
+
+export default async function handler(request, response) {
+  try {
+    const result =
+      await sql`CREATE TABLE Test ( Test1 varchar(255), Test2 varchar(255) );`;
+    return response.status(200).json({ result });
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+}
