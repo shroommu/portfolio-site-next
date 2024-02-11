92e8006b12b78c2e884b49ab1d723315118f732b
Alexa Kruckenberg <mouldy.k@gmail.com>
2024-02-11 09:24:43 -0800

    remove csv loader

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 module.exports = {
   trailingSlash: true,
   webpack(config) {
-    config.module.rules.push({
-      test: /\.csv$/,
-      use: [
-        {
-          loader: "csv-loader",
-          options: {
-            emitFile: true,
-          },
-        },
-      ],
-    });
 
     config.module.rules.push({
       test: /\.svg$/,
